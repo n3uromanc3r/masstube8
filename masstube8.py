9117f9f08ce70f028bed63461b17ed852bf6f1b3
keito <keito@neuromancer.(none)>
2014-04-12 22:02:17 +0100

    When you are wrestling for possession of a sword, the man with the handle always wins.

--- masstube8.py
+++ masstube8.py
@@ -17,25 +17,41 @@ def main():
 			response = s.get(url, headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0' })
 
 			soup = BeautifulSoup(response.text)
-			title = re.sub('[^\w\-_\. ]', '_', soup.title.string)
-
-			file_name = title +'.mp4'
-			name_check=True
-			count=1
-			while name_check:
-				if (os.path.isfile(file_name)):
-					file_name = title + '(' + str(count) + ').mp4'
-					count += 1
-				else:
-					name_check=False
+			title = re.sub('[^\w\-_\. ]', '_', soup.title.string)			
 
 			vid_url_match = re.search("(?<=videoUrlJS\t= ')[^}]*(?=',)", response.text)
 			vid_url = vid_url_match.group()
 
-			u = urllib2.urlopen(vid_url)
-			f = open(file_name, 'wb')
+			u = urllib2.urlopen(vid_url)			
 			meta = u.info()
 			file_size = int(meta.getheaders("Content-Length")[0])
+
+			file_name = title +'.mp4'
+			name_check=True
+			jump_to_next_video=False
+			count=1
+			while name_check:
+				if (os.path.isfile(file_name)):
+
+					f = open(file_name, "r")
+					current_file_size = len(f.read())
+					f.close()
+					if current_file_size == file_size:
+						print 'Skipped: ' + file_name + ' (identical file already exists)'
+						jump_to_next_video=True
+						name_check=False
+
+					file_name = title + '(' + str(count) + ').mp4'
+					count += 1
+
+				else:
+					name_check=False
+
+			if jump_to_next_video:
+				continue
+
+			f = open(file_name, 'wb')
+
 			print "\nDownloading: %s Bytes: %s" % (file_name, file_size)
 
 			file_size_dl = 0
